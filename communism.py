23598b3be9461b1ecd1e1e05dab4a5f7bd24d433
Joachim K <jokteur@gmail.com>
2022-01-15 11:37:08 +0100

    Prevent the failed conversion message to appear for builtins
    And rename the builtins to Python Intrinsic Global Structures (PIGS) in
    short. #1

--- communism.py
+++ communism.py
@@ -2,22 +2,25 @@ from typing import Iterable, Union
 
 
 def revolution(to_convert: Union[dict, Iterable]):
-    import inspect
+    import inspect, builtins
+    __PythonIntrinsicGlobalStructures__ = dir(builtins)
 
     def convert(c):
         from types import MethodType
 
         # We must protect the builtin elite to not be affected by the revolution
-        if inspect.isclass(c) and c.__name__ not in dir(__builtins__):
+        if inspect.isclass(c) and c.__name__ not in __PythonIntrinsicGlobalStructures__:
             try:
                 c.__eq__ = MethodType(lambda s, _: True, c)
                 c.__hash__ = MethodType(lambda s: hash(1), c)
             except:
                 print(f"Failed to convert {c} to communism.")
 
+
     to_convert = to_convert.values() if isinstance(to_convert, dict) else to_convert
     for m in to_convert:
         if inspect.ismodule(m):
             for c in vars(m).values():
                 convert(c)
-        convert(m)
+        else:
+            convert(m)
