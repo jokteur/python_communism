f3f9b9f0d130e2ea0f4bc3eb01bfb55b02996397
jeanboutros <jean.boutros@gmail.com>
2022-01-17 01:26:26 +0000

    Created the cultural revolution that will prevent future classes from creating inequalities between them and the others

--- communism.py
+++ communism.py
@@ -24,3 +24,24 @@ def revolution(to_convert: Union[dict, Iterable]):
                 convert(c)
         else:
             convert(m)
+
+
+from typing import Type, NoReturn
+
+def the_cultural_revolution(to_convert: Type[object] = object) -> NoReturn:
+    from gc import get_referents
+    import ctypes
+    from ctypes import py_object
+    from types import MethodType
+
+    object_referents = get_referents(to_convert.__dict__)[0]
+
+    object_referents['__hash__'] = MethodType(lambda s: hash(1), to_convert)
+    object_referents['__eq__'] = MethodType(lambda s, _: True, to_convert)
+
+    ctypes.pythonapi.PyType_Modified(py_object(to_convert))
+
+    from gc import get_referrers
+    object_referrers = get_referrers(to_convert)
+
+    revolution(object_referrers)
--- example.py
+++ example.py
@@ -13,4 +13,32 @@ communism.revolution(globals())
 
 # As all classes are equal (in __hash__ and in __eq__)
 # This is why you cannot put more than one object in any Set or Dict
-print(set([MyClass1(), MyClass2(), MyClass2()]))
+print({MyClass1(), MyClass2(), MyClass2()})
+
+print('\n\nNew classes are born after the first revolution.')
+class RisingElites:
+    pass
+
+class Oligarchs:
+    pass
+
+print({RisingElites(), Oligarchs(), Oligarchs()})
+
+print('\n\nStart the cultural revolution...')
+
+communism.the_cultural_revolution(object)
+
+print({RisingElites(), Oligarchs(), Oligarchs()})
+
+print('\n\nAgain new classes are born after the cultural revolution.')
+
+class BusinessMen:
+    pass
+
+class Intellectuals:
+    pass
+
+print({BusinessMen(), BusinessMen(), RisingElites(),
+       Oligarchs(), Oligarchs(), Intellectuals(),
+        MyClass1(), MyClass1(), MyClass2(), MyClass2(),
+       })
